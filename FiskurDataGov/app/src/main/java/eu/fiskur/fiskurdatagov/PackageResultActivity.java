9041d6c279f47c1f3b9c1ad8ecf782756a95d401
Jonathan Fisher <jonathan@fiskur.eu>
2015-01-19 16:21:28 +0000

    partially implmented google drive

--- FiskurDataGov/app/src/main/java/eu/fiskur/fiskurdatagov/PackageResultActivity.java
+++ FiskurDataGov/app/src/main/java/eu/fiskur/fiskurdatagov/PackageResultActivity.java
@@ -1,10 +1,16 @@
 package eu.fiskur.fiskurdatagov;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +20,19 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -28,6 +47,7 @@ public class PackageResultActivity extends ActionBarActivity {
     @InjectView(R.id.results_list_view) ListView resultsListView;
     PackageSearchResultObject resultObj;
     ArrayAdapter<PackageSearchResultObjectResource> resultResourceAdapter;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +64,69 @@ public class PackageResultActivity extends ActionBarActivity {
         }else{
             Timber.e("No extras object");
         }
+
+        connectDrive();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        l("Attempting to connect to GoogleDrive");
+        mGoogleApiClient.connect();
+    }
+
+    private static final int RESOLVE_CONNECTION_REQUEST_CODE = 9876;
+
+    private void connectDrive(){
+        l("Creating GoogleDrive client");
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        l("GoogleApiClient onConnected()");
+                        createRootFolder();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        l("GoogleApiClient onConnectionSuspended()");
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+                        l("GoogleApiClient onConnectionFailed()");
+                        if (connectionResult.hasResolution()) {
+                            try {
+                                connectionResult.startResolutionForResult(PackageResultActivity.this, RESOLVE_CONNECTION_REQUEST_CODE);
+                            } catch (IntentSender.SendIntentException e) {
+                                Timber.e(e.toString());
+                            }
+                        } else {
+                            Timber.e(connectionResult.toString());
+                            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), PackageResultActivity.this, 0).show();
+                        }
+                    }
+                })
+                .build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case RESOLVE_CONNECTION_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
+    }
+
+    private void l(String message){
+        Timber.d(message);
     }
 
     private void buildScreen(){
@@ -78,11 +161,33 @@ public class PackageResultActivity extends ActionBarActivity {
                 PackageSearchResultObjectResource resource = resultResourceAdapter.getItem(position - 1);
                 String url = resource.getUrl();
                 Timber.d("url: " + url);
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
+
+                if(url.endsWith(".xls")){
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(Uri.parse(url), "application/vnd.ms-excel");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+
+                    try {
+                        startActivity(intent);
+                    }
+                    catch (ActivityNotFoundException e) {
+                        Toast.makeText(PackageResultActivity.this, "No Application Available to View Excel", Toast.LENGTH_SHORT).show();
+                        launchBrowser(url);
+                    }
+                    return;
+                }else{
+                    launchBrowser(url);
+                }
+
             }
         });
+    }
+
+    private void launchBrowser(String url){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 
     public String append(String label, String section, String more, boolean first){
@@ -105,5 +210,19 @@ public class PackageResultActivity extends ActionBarActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createRootFolder(){
+        MetadataChangeSet changeSet = new MetadataChangeSet.Builder().setTitle("DataGovUk").build();
+        Drive.DriveApi.getRootFolder(mGoogleApiClient).createFolder(mGoogleApiClient, changeSet).setResultCallback(new ResultCallback<DriveFolder.DriveFolderResult>() {
+            @Override
+            public void onResult(DriveFolder.DriveFolderResult driveFolderResult) {
+                if (!driveFolderResult.getStatus().isSuccess()) {
+                    l("Error while trying to create the folder");
+                    return;
+                }
+                l("Created a folder: " + driveFolderResult.getDriveFolder().getDriveId());
+            }
+        });
     }
 }
